c509b8eb5a73683ff4a7c99cd5385d4915cd6b21
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2022-11-26 12:25:07 +0545

    added volume and music time functionality

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const musicArtist = document.querySelector(".track-artist");
 const musicAudio = document.querySelector(".music-audio");
 const progress = document.querySelector(".progress");
 const progressBar = document.querySelector(".progress-bar");
+const volume = document.querySelector(".volume");
+const volumeBar = document.querySelector(".volume-bar");
 const curTime = document.querySelector(".cur-time");
 const duration = document.querySelector(".duration-time");
 const prevBtn = document.querySelector("#prev");
@@ -14,6 +16,8 @@ const playPauseBtn = document.querySelector("#play-pause");
 const nextBtn = document.querySelector("#next");
 const shuffleBtn = document.querySelector("#shuffle");
 const repeatBtn = document.querySelector("#repeat");
+const volumeBtn = document.querySelector("#volume");
+
 //music array
 const musics = [
   { title: "brown munde", artist: "AP DHILLON" },
@@ -40,13 +44,22 @@ function musicChange(music) {
   musicArtist.textContent = music.artist.toUpperCase();
   musicImg.src = `./img/${music.title}.jpg`;
   musicAudio.src = `./songs/${music.title}.mp3`;
+
+  //for duration
+  setTimeout(() => {
+    durationTime.min = Math.floor(musicAudio.duration / 60);
+    durationTime.sec = Math.floor(musicAudio.duration % 60);
+    durationTime.sec =
+      durationTime.sec > 9 ? durationTime.sec : "0" + durationTime.sec;
+    duration.textContent = `${durationTime.min}:${durationTime.sec}`;
+  }, 10);
 }
 
 musicAudio.ontimeupdate = (e) => {
   let perc = (musicAudio.currentTime / musicAudio.duration) * 100;
-  console.log(musicAudio.duration);
-  console.log("perc = " + perc);
   progress.style.width = `${perc}%`;
+
+  //for current time
   currentTime.min = Math.floor(musicAudio.currentTime / 60);
   currentTime.sec = Math.floor(musicAudio.currentTime % 60);
   currentTime.sec =
@@ -147,8 +160,32 @@ progressBar.addEventListener("click", function (e) {
   // console.log(curTime);
 });
 
+volumeBtn.addEventListener("click", function () {
+  let ico = Array.from(document.querySelectorAll(".vol-ico"));
+  ico.forEach((el) => el.classList.toggle("hide"));
+  let icoVolume = ico[0]; //the icon with volume on
+  if (icoVolume.classList.contains("hide")) {
+    musicAudio.muted = true;
+  } else {
+    musicAudio.muted = false;
+  }
+  // console.log(icoVolume);
+});
+
+//volume bar
+volumeBar.addEventListener("click", function (e) {
+  let x = e.offsetX;
+  let volBarWidth = volumeBar.clientWidth;
+  let perc = (x / volBarWidth) * 100;
+  let vol = Math.floor(perc) / 100;
+  musicAudio.volume = vol;
+  console.log(Math.floor(perc) / 100);
+  volume.style.width = `${perc}%`;
+});
+
 function init() {
   songIndex = 0;
   musicChange(musics[songIndex]);
 }
+
 init();
